04bfcd9e3ca405c594a1184e884bd8908b686031
gabo180 <82070148+gabo180@users.noreply.github.com>
2021-06-23 14:04:35 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+// let React = require("react");
+import ReactDOM from "react-dom";
+// let ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
+
+// let h1 = document.createElement("h1");
+// h1.innerHTML = "Hello World!";
+// document.getElementById("root").appendChild(h1);
